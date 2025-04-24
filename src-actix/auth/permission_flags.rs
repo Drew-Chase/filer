61c07b691506a15d17064a173c6dd479db4948d6
Drew Chase (Home) <dcmanproductions@gmail.com>
2025-04-24 02:44:09 -0400

    Add PermissionFlags enum with default and utility methods
    Introduced the `PermissionFlags` enum to represent permission levels using bitflags. Included a default implementation and an `all` method to return all combined permissions as a single value. This provides a flexible and efficient way to manage user permissions.

---
+++ src-actix/auth/permission_flags.rs
@@ -0,0 +1,27 @@
+use enumflags2::{BitFlags, bitflags};
+use serde::{Deserialize, Serialize};
+
+#[bitflags]
+#[repr(u8)]
+#[derive(Debug, Copy, Clone, PartialEq, Serialize, Deserialize)]
+pub enum PermissionFlags {
+    Read = 0b00000001,
+    Write = 0b00000010,
+    Delete = 0b00000100,
+    Create = 0b00001000,
+    Upload = 0b00010000,
+    Download = 0b00100000,
+}
+
+impl Default for PermissionFlags {
+    fn default() -> Self {
+        Self::Read
+    }
+}
+
+impl PermissionFlags {
+    pub fn all() -> u8 {
+        (Self::Read | Self::Write | Self::Delete | Self::Create | Self::Upload | Self::Download)
+            .bits_c()
+    }
+}
