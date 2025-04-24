b67e8dbb9323b0a094b6b2c1463b46a8c626e5c5
Drew Chase (Home) <dcmanproductions@gmail.com>
2025-04-23 21:24:01 -0400

    Refactor Actix module structure and update file paths
    Moved `http_error` and `asset_endpoint` modules to a new `helpers` directory for better organization. Updated `WWWROOT.get_file` path in `index` function to correctly locate the `index.html` file relative to the new structure. Fixed minor typos in comments for improved clarity.

--- src-actix/helpers/asset_endpoint.rs
+++ src-actix/helpers/asset_endpoint.rs
@@ -3,7 +3,7 @@ use actix_files::file_extension_to_mime;
 use actix_web::error::ErrorInternalServerError;
 use actix_web::{get, web, App, Error, HttpRequest, HttpResponse, Responder};
 use include_dir::{include_dir, Dir};
-use vite_actix::ViteAppFactory;
+use vite_actix::vite_app_factory::ViteAppFactory;
 
 // Static directory including all files under `target/wwwroot`.
 //
@@ -15,7 +15,7 @@ static WWWROOT: Dir = include_dir!("target/wwwroot");
 // Handles the request for the index.html file.
 //
 // This function serves the `index.html` file from the embedded directory
-// if it exists, and returns an internal server error if the file is not found.
+// if it exists and returns an internal server error if the file is not found.
 //
 // # Arguments
 //
@@ -23,10 +23,10 @@ static WWWROOT: Dir = include_dir!("target/wwwroot");
 //
 // # Returns
 //
-// An `impl Responder` which can either be a successful HTTP response containing
+// An `impl Responder,` which can either be a successful HTTP response containing
 // the `index.html` file, or an internal server error.
 pub async fn index(_req: HttpRequest) -> anyhow::Result<impl Responder, Error> {
-	if let Some(file) = WWWROOT.get_file("index.html") {
+	if let Some(file) = WWWROOT.get_file("../../index.html") {
 		let body = file.contents();
 		return Ok(HttpResponse::Ok().content_type("text/html").body(body));
 	}
---
+++ src-actix/helpers/mod.rs
@@ -0,0 +1,2 @@
+pub(crate) mod asset_endpoint;
+pub(crate) mod http_error;
