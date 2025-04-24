38eecdb0e1c33c7060b875e8aa7b9205ff79f365
Drew Chase (Home) <dcmanproductions@gmail.com>
2025-04-24 02:44:39 -0400

    Add filesystem data and entry structures with serde support
    Introduce `FilesystemEntry` and `FilesystemData` structs to represent filesystem information, both implementing `serde::Serialize`. Provide `From<PathBuf>` implementations to populate these structs, enabling easy transformation of filesystem paths into structured data.

---
+++ src-actix/filesystem/filesystem_data.rs
@@ -0,0 +1,44 @@
+use serde::Serialize;
+use std::path::{Path, PathBuf};
+
+#[derive(Serialize)]
+pub struct FilesystemEntry {
+    pub filename: String,
+    pub path: String,
+    pub size: u64,
+    pub is_dir: bool,
+}
+
+#[derive(Serialize)]
+pub struct FilesystemData {
+    pub parent: Option<String>,
+    pub entries: Vec<FilesystemEntry>,
+}
+
+impl From<PathBuf> for FilesystemEntry {
+    fn from(path: PathBuf) -> Self {
+        let metadata = path.metadata().unwrap();
+        FilesystemEntry {
+            filename: path.file_name().unwrap().to_str().unwrap().to_string(),
+            path: path.to_str().unwrap().to_string(),
+            size: metadata.len(),
+            is_dir: metadata.is_dir(),
+        }
+    }   
+}
+
+impl From<PathBuf> for FilesystemData {
+    fn from(path: PathBuf) -> Self {
+        let readdir = std::fs::read_dir(&path).unwrap();
+        let mut entries = Vec::new();
+        for entry in readdir {
+            let entry = entry.unwrap();
+            let path = entry.path();
+            entries.push(path.into());
+        }
+        FilesystemData {
+            parent: path.parent().map(|p| p.to_str().unwrap().to_string()),
+            entries,
+        }
+    }
+}
