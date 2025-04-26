06d9ee2067577c55dbec3cf4834a94af2bbb7d73
Drew Chase (Home) <dcmanproductions@gmail.com>
2025-04-25 22:16:31 -0400

    Refactor FilesPage with new UI components and upload logic.
    Replaced custom file path logic with `FileTableBreadcrumbs` and `useFileSystemEntry`. Added file upload functionality with progress tracking and improved UI with buttons for common actions. Simplified and modernized the page structure for better maintainability.

--- src/assets/pages/FilesPage.tsx
+++ src/assets/pages/FilesPage.tsx
@@ -1,35 +1,118 @@
-import {useLocation, useNavigate} from "react-router-dom";
-import {useEffect, useState} from "react";
-import {useAuth} from "../providers/AuthProvider.tsx";
-import {Table} from "@heroui/react";
+import {useState} from "react";
+import FileTable from "../components/FileTable.tsx";
+import FileTableBreadcrumbs from "../components/FileTableBreadcrumbs.tsx";
+import {useFileSystemEntry} from "../providers/FileSystemEntryProvider.tsx";
+import {Alert, Button, ButtonGroup, CircularProgress, Tooltip} from "@heroui/react";
+import fs from "../ts/filesystem.ts";
+import {Icon} from "@iconify-icon/react";
+
+type FileUploadData = {
+    name: string;
+    progress: number;
+}
 
 export default function FilesPage()
 {
-    const {isLoggedIn} = useAuth();
-    const {pathname} = useLocation();
-    const [filePath, setFilePath] = useState("");
-    const navigate = useNavigate();
-    useEffect(() =>
-    {
-        let path = pathname
-            .replace("/files/", "")  // Remove the /files/ prefix
-            .replace(/^\//, "");     // Remove leading slash if present
-        if (path == "")
-            path = "/"
-        console.log(path);
-        setFilePath(path);
-    }, [pathname]);
-    useEffect(() =>
-    {
-        if (!isLoggedIn)
-            navigate("/");
-    }, [isLoggedIn]);
-    return (
-        <>
-            <h1 className="text-3xl font-bold underline">FilePath is {filePath}</h1>
-            <Table>
+    const [fileUploadData, setFileUploadData] = useState<FileUploadData | null>(null);
+    const {currentPath, refresh, loading} = useFileSystemEntry();
 
-            </Table>
-        </>
+    return (
+        <div className="container p-4 h-[calc(100dvh_-_100px)] w-[calc(100vw_-_50px)] mx-auto rounded-lg max-w-[unset] bg-white/5">
+            {currentPath !== null && (
+                <div className={"flex flex-col w-full gap-4"}>
+                    <div className={"flex flex-row gap-4 items-center justify-between"}>
+                        <FileTableBreadcrumbs paths={currentPath.split("/")}/>
+                        <ButtonGroup>
+                            <Tooltip content={"Upload File"}>
+                                <Button
+                                    variant={"ghost"}
+                                    size={"sm"}
+                                    className={"text-xl"}
+                                    onPress={() =>
+                                    {
+                                        const input = document.createElement("input");
+                                        input.type = "file";
+                                        input.multiple = false;
+                                        input.click();
+                                        input.onchange = async () =>
+                                        {
+                                            const file = input.files?.[0];
+                                            if (!file)
+                                                return;
+                                            await fs.upload(file, currentPath, bytes =>
+                                            {
+                                                setFileUploadData({name: file.name, progress: bytes / file.size});
+                                            });
+                                            setTimeout(() =>
+                                            {
+                                                setFileUploadData(null);
+                                            }, 5 * 1000);
+                                        };
+
+                                    }}
+                                >
+                                    <Icon icon={"mage:file-upload-fill"}/>
+                                </Button>
+                            </Tooltip>
+                            <Tooltip content={"Archive and Download"}>
+                                <Button
+                                    size={"sm"}
+                                    variant={"ghost"}
+                                    className={"text-xl"}
+                                >
+                                    <Icon icon={"mage:archive-fill"}/>
+                                </Button>
+                            </Tooltip>
+                            <Tooltip content={"Create New Directory"}>
+                                <Button
+                                    size={"sm"}
+                                    variant={"ghost"}
+                                    className={"text-xl"}
+                                >
+                                    <Icon icon={"mage:folder-plus-fill"}/>
+                                </Button>
+                            </Tooltip>
+                            <Tooltip content={"Create New File"}>
+                                <Button
+                                    size={"sm"}
+                                    variant={"ghost"}
+                                    className={"text-xl"}
+                                >
+                                    <Icon icon={"mage:file-plus-fill"}/>
+                                </Button>
+                            </Tooltip>
+                            <Tooltip content={"Refresh"}>
+                                <Button
+                                    size={"sm"}
+                                    variant={"ghost"}
+                                    className={"text-xl"}
+                                    onPress={refresh}
+                                    isLoading={loading}
+                                >
+                                    {!loading && <Icon icon={"mage:refresh"}/>}
+                                </Button>
+                            </Tooltip>
+                        </ButtonGroup>
+                    </div>
+                    <FileTable/>
+                </div>
+            )}
+            {fileUploadData && (
+                <Alert
+                    className={"absolute w-fit bottom-5 right-5"}
+                    classNames={{
+                        base: "bg-primary/10 backdrop-blur-sm"
+                    }}
+                    variant={"solid"}
+                    icon={<CircularProgress minValue={0} maxValue={1} value={fileUploadData.progress} color={"primary"}/>}
+                    title={
+                        <Tooltip content={`Uploading: ${fileUploadData.name}`} delay={1000}>
+                            <p className={"max-w-[250px] truncate"}>Uploading: {fileUploadData.name}</p>
+                        </Tooltip>
+                    }
+                    description={`Uploading file ${(fileUploadData.progress * 100).toFixed(2)}%...`}
+                />
+            )}
+        </div>
     );
 }
