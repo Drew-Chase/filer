1a7f9444179df39481ee62565090a7e86f421b25
Drew Chase (Home) <dcmanproductions@gmail.com>
2025-04-25 22:16:55 -0400

    Add FileTableBreadcrumbs component for navigation
    Introduces a breadcrumb component using HeroUI to enable easier navigation within the file system. The component dynamically builds breadcrumbs from the provided paths and integrates navigation functionality through the FileSystemEntryProvider.

---
+++ src/assets/components/FileTableBreadcrumbs.tsx
@@ -0,0 +1,25 @@
+import {BreadcrumbItem, Breadcrumbs} from "@heroui/react";
+import {useFileSystemEntry} from "../providers/FileSystemEntryProvider.tsx";
+
+export default function FileTableBreadcrumbs({paths}: { paths: string[] })
+{
+    const {navigate} = useFileSystemEntry();
+    return (
+        <Breadcrumbs variant={"bordered"}>
+            {paths.map((path, index) => (
+                <BreadcrumbItem
+                    key={path}
+                    onPress={() =>
+                    {
+                        if (index >= paths.length)
+                            return;
+                        let newPath = paths.slice(0, index + 1).join("/");
+                        navigate(newPath);
+                    }}
+                >
+                    {path}
+                </BreadcrumbItem>
+            ))}
+        </Breadcrumbs>
+    );
+}
