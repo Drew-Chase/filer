8d85ee81a68dda29ac84c575d25700aedc6dd3c3
Drew Chase (Home) <dcmanproductions@gmail.com>
2025-04-28 08:13:55 -0400

    Add RenameModal component for renaming files and folders
    This commit introduces the RenameModal component, which provides UI functionality to rename filesystem entries. It includes an input field for the new filename, validation, and interaction with the filesystem provider to handle renaming logic.

---
+++ src/assets/components/modals/RenameModal.tsx
@@ -0,0 +1,62 @@
+import {Button, Input, Modal, ModalBody, ModalContent, ModalFooter, ModalHeader} from "@heroui/react";
+import {FilesystemEntry} from "../../ts/filesystem.ts";
+import {Icon} from "@iconify-icon/react";
+import {useEffect, useState} from "react";
+import {useFileSystemEntry} from "../../providers/FileSystemEntryProvider.tsx";
+
+type RenameProperties = {
+    entry: FilesystemEntry | null;
+    onClose: () => void;
+};
+
+export default function RenameModal(props: RenameProperties)
+{
+    const {moveEntry} = useFileSystemEntry();
+    const [filename, setFilename] = useState<string>(props.entry?.filename || "");
+
+    useEffect(() =>
+    {
+        if (props.entry === null) return;
+        setFilename(props.entry.filename);
+
+    }, [props.entry]);
+
+    return (
+        <Modal isOpen={props.entry !== null} onClose={props.onClose}>
+            <ModalContent>
+                {onClose => (
+                    <>
+                        <ModalHeader>Rename</ModalHeader>
+                        <ModalBody>
+                            <Input
+                                label={"Filename"}
+                                size={"sm"}
+                                autoFocus={true}
+                                value={filename}
+                                onValueChange={setFilename}
+                                endContent={<Icon icon={"gg:rename"} width={18} className={"opacity-50 my-auto"}/>}
+                                classNames={{
+                                    inputWrapper: "bg-white/20 data-[hover]:bg-white/15 group-data-[focus]:bg-white/10 group-data-[focus]:border-primary border-1 border-transparent"
+                                }}
+                            />
+                        </ModalBody>
+                        <ModalFooter>
+                            <Button onPress={() =>
+                            {
+                                if (props.entry?.filename === filename)
+                                {
+                                    onClose();
+                                    return;
+                                }
+                                let oldPath = props.entry?.path ?? "";
+                                let newFilePath = `${oldPath.substring(0, oldPath.lastIndexOf("/"))}/${filename}`;
+                                console.log("New File Path", newFilePath);
+                            }}>Rename</Button>
+                            <Button onPress={onClose} variant={"light"} color={"danger"}>Cancel</Button>
+                        </ModalFooter>
+                    </>
+                )}
+            </ModalContent>
+        </Modal>
+    );
+}
