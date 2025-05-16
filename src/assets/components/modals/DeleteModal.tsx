ac47adff085b5c705bb84f6a428d0043d23a9135
Drew Chase (Work) <dcmanproductions@gmail.com>
2025-05-16 15:05:14 -0400

    Improve DeleteModal functionality and enhance user feedback
    Updated DeleteModal with loading state on delete action and refined deletion confirmation message to include the entry name. Integrated file system refresh logic and adjusted modal behavior for better user experience.

--- src/assets/components/modals/DeleteModal.tsx
+++ src/assets/components/modals/DeleteModal.tsx
@@ -1,23 +1,36 @@
 import {Button, Modal, ModalBody, ModalContent, ModalFooter, ModalHeader} from "@heroui/react";
-import {FilesystemEntry} from "../../ts/filesystem.ts";
+import {FileSystem, FilesystemEntry} from "../../ts/filesystem.ts";
+import {useState} from "react";
+import {useFileSystemEntry} from "../../providers/FileSystemEntryProvider.tsx";
 
 type DeleteProperties = {
     entry: FilesystemEntry | null;
-    onClose: (confirm: boolean) => void;
+    onClose: () => void;
 };
 
 export default function DeleteModal(props: DeleteProperties)
 {
+    const [isLoading, setIsLoading] = useState(false);
+    const {refresh} = useFileSystemEntry();
     return (
-        <Modal isOpen={props.entry !== null}>
+        <Modal isOpen={props.entry !== null} onClose={props.onClose} backdrop={"blur"}>
             <ModalContent>
                 <ModalHeader>Delete</ModalHeader>
-                <ModalBody>
-                    Are you sure you want to delete this?
+                <ModalBody className={"flex flex-row"}>
+                    <p>
+                        Are you sure you want to delete <span className={"font-bold italic inline"}>{props.entry?.filename}</span> and all of its contents? This action cannot be undone.
+                    </p>
                 </ModalBody>
                 <ModalFooter>
-                    <Button color={"primary"} onPress={() => props.onClose(true)}>Delete</Button>
-                    <Button variant={"light"} color={"danger"} onPress={() => props.onClose(false)}>Cancel</Button>
+                    <Button isLoading={isLoading} onPress={async () =>
+                    {
+                        setIsLoading(true);
+                        await FileSystem.deleteEntry(props.entry?.path || "");
+                        refresh();
+                        props.onClose();
+                        setIsLoading(false);
+                    }}>Delete</Button>
+                    <Button variant={"light"} color={"danger"} onPress={props.onClose}>Cancel</Button>
                 </ModalFooter>
             </ModalContent>
         </Modal>
