fea823a13591099deec93e906ee26d4b9d085732
Drew Chase (Work) <dcmanproductions@gmail.com>
2025-05-22 15:45:59 -0400

    Update UploadEntryModal to display upload speed and refresh state
    Added real-time upload speed tracking to the modal header and implemented a state refresh upon upload completion using `useFileSystemEntry`. Enhanced user feedback and streamlined the upload process for better user experience.

--- src/assets/components/modals/UploadEntryModal.tsx
+++ src/assets/components/modals/UploadEntryModal.tsx
@@ -2,6 +2,8 @@ import {Modal, ModalBody, ModalContent, ModalHeader} from "@heroui/react";
 import DragDropArea from "../upload/DragDropArea.tsx";
 import {useState} from "react";
 import FileUploadProgress from "../upload/FileUploadProgress.tsx";
+import {useFileSystemEntry} from "../../providers/FileSystemEntryProvider.tsx";
+import {FileSystem} from "../../ts/filesystem.ts";
 
 type UploadEntryProperties = {
     isOpen: boolean;
@@ -11,6 +13,8 @@ type UploadEntryProperties = {
 export default function UploadEntryModal(props: UploadEntryProperties)
 {
     const [uploadFiles, setUploadFiles] = useState<File[]>([]);
+    const [uploadSpeed, setUploadSpeed] = useState(0);
+    const {refresh} = useFileSystemEntry();
     return (
         <Modal
             isOpen={props.isOpen}
@@ -30,11 +34,23 @@ export default function UploadEntryModal(props: UploadEntryProperties)
             }}
         >
             <ModalContent>
-                <ModalHeader>Upload Files</ModalHeader>
+                <ModalHeader className={"flex flex-row items-center"}>
+                    <div className={"w-full"}>Upload Files</div>
+                    {(uploadFiles.length > 0 && uploadSpeed !== 0 && !isNaN(uploadSpeed)) ? (<div className={"w-full text-sm text-nowrap text-end"}>{FileSystem.formatSize(uploadSpeed)}/s</div>) : (<></>)}
+                </ModalHeader>
                 <ModalBody>
                     {uploadFiles.length === 0 ?
                         <DragDropArea onFileSelected={setUploadFiles}/> :
-                        <FileUploadProgress files={uploadFiles}/>
+                        <FileUploadProgress
+                            onUploadSpeedChange={setUploadSpeed}
+                            files={uploadFiles}
+                            onUploadComplete={() =>
+                            {
+                                refresh();
+                                setUploadFiles([]);
+                                props.onClose();
+                            }}
+                        />
                     }
                 </ModalBody>
             </ModalContent>
