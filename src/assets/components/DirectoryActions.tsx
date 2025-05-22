5f310d1e5ce5fbb984b4c1681956c3e0667665fb
Drew Chase <dcmanproductions@gmail.com>
2025-05-22 18:33:57 -0400

    Refactor DirectoryActions to use reusable toggle logic
    Replaced inline toggle logic with a reusable `toggleAlwaysShow` function, improving code readability and maintainability. Added a spinner and loading state to buttons for better user feedback. Updated button indexing and animations to reflect the new additions and ensure smoother UI transitions.

--- src/assets/components/DirectoryActions.tsx
+++ src/assets/components/DirectoryActions.tsx
@@ -1,12 +1,13 @@
 import {motion} from "framer-motion";
-import {useState} from "react";
+import {useCallback, useState} from "react";
 import {Icon} from "@iconify-icon/react";
-import {addToast, cn, Tooltip} from "@heroui/react";
+import {addToast, cn, Spinner, Tooltip} from "@heroui/react";
 import {useFileSystemEntry} from "../providers/FileSystemEntryProvider.tsx";
 
 export function DirectoryActions()
 {
     const {
+        loading,
         selectedEntries,
         downloadSelected,
         askDeleteSelectedEntries,
@@ -18,6 +19,17 @@ export function DirectoryActions()
     // Add a state to track if the parent container is being hovered
     const [isHovering, setIsHovering] = useState(false);
     const [alwaysShow, setAlwaysShow] = useState(localStorage.getItem("alwaysShowDirectoryActions") === "true");
+
+    const toggleAlwaysShow = useCallback(() =>
+    {
+        const value = !alwaysShow;
+        addToast({
+            title: "Auto collapse toggled",
+            description: value ? "Auto collapse is now enabled." : "Auto collapse is now disabled."
+        });
+        localStorage.setItem("alwaysShowDirectoryActions", value.toString());
+        setAlwaysShow(value);
+    }, [alwaysShow]);
 
     return (
         <motion.div
@@ -34,12 +46,7 @@ export function DirectoryActions()
                         description: "You can double-click to toggle the directory actions menu."
                     });
                 }
-                setAlwaysShow(prev =>
-                {
-                    const value = !prev;
-                    localStorage.setItem("alwaysShowDirectoryActions", value.toString());
-                    return value;
-                });
+                toggleAlwaysShow();
             }}
         >
             <DirectoryActionButton
@@ -49,7 +56,7 @@ export function DirectoryActions()
                 onPress={downloadSelected}
                 isPositiveCountRequired={true}
                 isVisible={alwaysShow || isHovering}
-                index={6}
+                index={7}
             />
             <DirectoryActionButton
                 count={selectedEntries.size}
@@ -58,7 +65,7 @@ export function DirectoryActions()
                 onPress={downloadSelected}
                 isPositiveCountRequired={true}
                 isVisible={alwaysShow || isHovering}
-                index={5}
+                index={6}
             />
             <DirectoryActionButton
                 count={selectedEntries.size}
@@ -67,7 +74,7 @@ export function DirectoryActions()
                 onPress={downloadSelected}
                 isPositiveCountRequired={true}
                 isVisible={alwaysShow || isHovering}
-                index={4}
+                index={5}
             />
             <DirectoryActionButton
                 count={selectedEntries.size}
@@ -77,7 +84,7 @@ export function DirectoryActions()
                 isPositiveCountRequired={true}
                 isVisible={alwaysShow || isHovering}
                 color={"danger"}
-                index={3}
+                index={4}
             />
 
             <DirectoryActionButton
@@ -87,7 +94,7 @@ export function DirectoryActions()
                 isPositiveCountRequired={false}
                 showCount={false}
                 isVisible={alwaysShow || isHovering}
-                index={2}
+                index={3}
             />
 
             <DirectoryActionButton
@@ -97,7 +104,7 @@ export function DirectoryActions()
                 isPositiveCountRequired={false}
                 showCount={false}
                 isVisible={alwaysShow || isHovering}
-                index={1}
+                index={2}
             />
             <DirectoryActionButton
                 icon={"tabler:reload"}
@@ -106,7 +113,17 @@ export function DirectoryActions()
                 showCount={false}
                 isPositiveCountRequired={false}
                 isVisible={alwaysShow || isHovering}
-                color={"primary"}
+                index={1}
+                isLoading={loading}
+            />
+            <DirectoryActionButton
+                icon={"icon-park-solid:right-expand"}
+                tooltip={"Toggle auto collapse"}
+                onPress={toggleAlwaysShow}
+                showCount={false}
+                isPositiveCountRequired={false}
+                color={alwaysShow ? "primary" : "default"}
+                isVisible={alwaysShow || isHovering}
                 index={0}
             />
         </motion.div>
@@ -123,6 +140,7 @@ type DirectoryActionButtonProps = {
     isVisible: boolean;
     color?: "primary" | "danger" | "default";
     index: number;
+    isLoading?: boolean;
 }
 
 function DirectoryActionButton(props: DirectoryActionButtonProps)
@@ -132,69 +150,75 @@ function DirectoryActionButton(props: DirectoryActionButtonProps)
     const isPositiveCountRequired = props.isPositiveCountRequired ?? false;
     const showCount = props.showCount ?? true;
     const color = props.color ?? "default";
+    const isLoading = props.isLoading ?? false;
 
     if (props.count === 0 && isPositiveCountRequired) return null;
 
     return (
-        <Tooltip content={props.tooltip} closeDelay={0} delay={1500}>
-            <motion.div
-                id={id}
-                className={
-                    cn(
-                        "flex items-center justify-center aspect-square w-10 h-10 rounded-full relative",
-                        "drop-shadow-md shadow-sm",
-                        "data-[hover=true]:brightness-95 data-[hover=true]:text-black cursor-pointer",
-                        "data-[color=primary]:bg-primary data-[color=primary]:text-white",
-                        "data-[color=danger]:bg-danger data-[color=danger]:text-white",
-                        "data-[color=default]:bg-white data-[color=default]:text-black"
-                    )
-                }
-                initial={{
-                    x: (props.index === 0 ? 0 : 43 * props.index),
-                    scale: (props.index === 0 ? 1 : 0.8),
-                    opacity: (props.index === 0 ? 1 : 0.5)
-                }}
-                animate={{
-                    x: props.isVisible ? 0 : (props.index === 0 ? 0 : 43 * props.index),
-                    scale: props.isVisible ? 1 : (props.index === 0 ? 1 : 0.8),
-                    opacity: props.isVisible ? 1 : (props.index === 0 ? 1 : props.index < 5 ? 0.5 : 0)
-                }}
-                transition={{
-                    duration: 0.3,
-                    delay: props.isVisible ? props.index * 0.1 : 0,
-                    type: "spring",
-                    stiffness: 400,
-                    damping: 20
-                }}
-                onMouseEnter={() => setHovering(true)}
-                onMouseLeave={() => setHovering(false)}
-                onClick={props.onPress}
-                data-hover={hovering}
-                data-color={color}
-            >
-                {showCount && (
-                    <motion.div
-                        key={props.count}
-                        initial={{scale: 1, y: -5}}
-                        animate={{scale: 1, y: 0}}
-                        transition={{
-                            type: "spring",
-                            stiffness: 300,
-                            damping: 10
-                        }}
-                        className={
-                            cn(
-                                "absolute top-0 right-0 -mt-2 -mr-2 z-20 p-1 aspect-square w-6 h-6",
-                                "bg-white text-black text-xs rounded-full flex items-center justify-center"
-                            )
-                        }
-                        data-hover={hovering}
-                    >
-                        {props.count}
-                    </motion.div>
-                )}
-                <Icon icon={props.icon}/>
-            </motion.div>
-        </Tooltip>
+        <div className={"relative"}>
+            {isLoading && (<Spinner size={"lg"} color={"primary"} className={"absolute top-0 left-0 z-10"}/>)}
+            <Tooltip content={props.tooltip} closeDelay={0} delay={1500}>
+                <motion.div
+                    id={id}
+                    className={
+                        cn(
+                            "flex items-center justify-center aspect-square w-10 h-10 rounded-full relative",
+                            "drop-shadow-md shadow-sm",
+                            "data-[hover=true]:brightness-95 data-[hover=true]:text-black cursor-pointer",
+                            "data-[color=primary]:bg-primary data-[color=primary]:text-white",
+                            "data-[color=danger]:bg-danger data-[color=danger]:text-white",
+                            "data-[color=default]:bg-white data-[color=default]:text-black"
+                        )
+                    }
+                    initial={{
+                        x: (props.index === 0 ? 0 : 43 * props.index),
+                        scale: (props.index === 0 ? 1 : 0.8),
+                        opacity: (props.index === 0 ? 1 : 0.5),
+                        rotate: 360
+                    }}
+                    animate={{
+                        x: props.isVisible ? 0 : (props.index === 0 ? 0 : 43 * props.index),
+                        scale: props.isVisible ? 1 : (props.index === 0 ? 1 : 0.8),
+                        opacity: props.isLoading ? 0 : (props.isVisible ? 1 : (props.index === 0 ? 1 : props.index < 5 ? 0.5 : 0)),
+                        rotate: props.isLoading ? 360 : 0
+                    }}
+                    transition={{
+                        duration: 0.3,
+                        delay: props.isVisible ? props.index * 0.051 : 0,
+                        type: "spring",
+                        stiffness: 400,
+                        damping: 20
+                    }}
+                    onMouseEnter={() => setHovering(true)}
+                    onMouseLeave={() => setHovering(false)}
+                    onClick={props.onPress}
+                    data-hover={hovering}
+                    data-color={color}
+                >
+                    {showCount && (
+                        <motion.div
+                            key={props.count}
+                            initial={{scale: 1, y: -5}}
+                            animate={{scale: 1, y: 0}}
+                            transition={{
+                                type: "spring",
+                                stiffness: 300,
+                                damping: 10
+                            }}
+                            className={
+                                cn(
+                                    "absolute top-0 right-0 -mt-2 -mr-2 z-20 p-1 aspect-square w-6 h-6",
+                                    "bg-white text-black text-xs rounded-full flex items-center justify-center"
+                                )
+                            }
+                            data-hover={hovering}
+                        >
+                            {props.count}
+                        </motion.div>
+                    )}
+                    <Icon icon={props.icon}/>
+                </motion.div>
+            </Tooltip>
+        </div>
     );
 }
