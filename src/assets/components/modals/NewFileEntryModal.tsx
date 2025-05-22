a83b463ef0eb7a0a1b50c224de640473f1ba61ec
Drew Chase <dcmanproductions@gmail.com>
2025-05-22 18:34:22 -0400

    Add support for creating directories in NewFileEntryModal
    Introduced a checkbox to toggle between file and directory creation. Integrated functionality to handle directory creation in the FileSystem and display relevant toasts upon failure. Updated modal structure and button actions to support the new feature.

--- src/assets/components/modals/NewFileEntryModal.tsx
+++ src/assets/components/modals/NewFileEntryModal.tsx
@@ -1,7 +1,8 @@
-import {Button, Input, Modal, ModalBody, ModalContent, ModalFooter, ModalHeader} from "@heroui/react";
+import {addToast, Button, Checkbox, Input, Modal, ModalBody, ModalContent, ModalFooter, ModalHeader} from "@heroui/react";
 import {Icon} from "@iconify-icon/react";
 import {useFileSystemEntry} from "../../providers/FileSystemEntryProvider.tsx";
 import {useState} from "react";
+import {FileSystem} from "../../ts/filesystem.ts";
 
 type NewFileEntryProperties = {
     isOpen: boolean;
@@ -10,9 +11,19 @@ type NewFileEntryProperties = {
 
 export default function NewFileEntryModal(props: NewFileEntryProperties)
 {
-    const {} = useFileSystemEntry();
+    const {currentPath, refresh} = useFileSystemEntry();
     const [filename, setFilename] = useState<string>("");
+    const [isDirectory, setIsDirectory] = useState(false);
     const [isLoading, setIsLoading] = useState(false);
+    if (currentPath == null) return null;
+
+    const reset = () =>
+    {
+        setFilename("");
+        setIsDirectory(false);
+        setIsLoading(false);
+    };
+
     return (
         <Modal
             isOpen={props.isOpen}
@@ -23,29 +34,57 @@ export default function NewFileEntryModal(props: NewFileEntryProperties)
             }}
         >
             <ModalContent>
-                {onClose => (
-                    <>
-                        <ModalHeader>New File or Directory</ModalHeader>
-                        <ModalBody>
-                            <Input
-                                label={"Filename"}
-                                size={"sm"}
-                                autoFocus={true}
-                                value={filename}
-                                onValueChange={setFilename}
-                                endContent={<Icon icon={"gg:rename"} width={18} className={"opacity-50 my-auto"}/>}
-                                classNames={{
-                                    inputWrapper: "bg-white/20 data-[hover]:bg-white/15 group-data-[focus]:bg-white/10 group-data-[focus]:outline-primary outline-2 border-transparent"
-                                }}
-                            />
-                        </ModalBody>
-                        <ModalFooter>
-                            <Button onPress={onClose} variant={"solid"} color={"secondary"}>New File</Button>
-                            <Button onPress={onClose} variant={"ghost"} color={"secondary"}>New Directory</Button>
-                            <Button onPress={onClose} variant={"light"} color={"danger"}>Cancel</Button>
-                        </ModalFooter>
-                    </>
-                )}
+                <ModalHeader>New File or Directory</ModalHeader>
+                <ModalBody>
+                    <Input
+                        label={"Filename"}
+                        size={"sm"}
+                        autoFocus={true}
+                        value={filename}
+                        onValueChange={setFilename}
+                        endContent={<Icon icon={"gg:rename"} width={18} className={"opacity-50 my-auto"}/>}
+                        classNames={{
+                            inputWrapper: "bg-white/20 data-[hover]:bg-white/15 group-data-[focus]:bg-white/10 group-data-[focus]:outline-primary outline-2 border-transparent"
+                        }}
+                    />
+                    <div className={"flex flex-row items-center justify-between cursor-pointer hover:bg-white/10 p-2 rounded-md"} onClick={() => setIsDirectory(!isDirectory)}>
+                        <p>Create Directory</p>
+                        <Checkbox isSelected={isDirectory} onValueChange={setIsDirectory} radius={"full"}/>
+                    </div>
+                </ModalBody>
+                <ModalFooter>
+                    <Button
+                        onPress={async () =>
+                        {
+                            setIsLoading(true);
+                            try
+                            {
+                                await FileSystem.createEntry(filename, currentPath, isDirectory);
+                            } catch (e: Error | any)
+                            {
+                                addToast({
+                                    title: "Failed to create entry",
+                                    description: e.message || e.toString() || "Unknown error occurred while trying to create the entry.",
+                                    color: "danger"
+                                });
+                            }
+                            refresh();
+                            props.onClose();
+                            setIsLoading(false);
+                            reset();
+                        }}
+                        variant={"solid"}
+                        color={"secondary"}
+                        isLoading={isLoading}
+                    >
+                        New {isDirectory ? "Directory" : "File"}
+                    </Button>
+                    <Button onPress={() =>
+                    {
+                        reset();
+                        props.onClose();
+                    }} variant={"light"} color={"danger"}>Cancel</Button>
+                </ModalFooter>
             </ModalContent>
         </Modal>
     );
