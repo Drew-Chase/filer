1c70f5cbd6c40be692db33784c2e285275e0051d
Drew Chase <dcmanproductions@gmail.com>
2025-05-24 17:50:42 -0400

    Add argument parsing and configuration file support
    Introduced `FilerArguments` for runtime argument parsing using `clap` and integrated a JSON-based configuration file system. Replaced hardcoded `PORT` with dynamic port handling based on arguments or configuration. Updated file indexing and watcher logic to allow disabling via arguments or configuration flags.

--- src-actix/lib.rs
+++ src-actix/lib.rs
@@ -1,10 +1,14 @@
+use crate::arguments::FilerArguments;
 use crate::auth::{auth_db, auth_endpoint};
+use crate::configuration::Configuration;
 use crate::helpers::asset_endpoint::AssetsAppConfig;
-use crate::helpers::constants::{DEBUG, PORT};
+use crate::helpers::constants::DEBUG;
 use crate::io::fs::indexer::indexer_data::IndexerData;
 use crate::io::fs::indexer::{indexer_data, indexer_db};
 use actix_web::{App, HttpResponse, HttpServer, middleware, web};
 use anyhow::Result;
+use clap::Parser;
+use include_dir::{Dir, include_dir};
 use io::fs::filesystem_endpoint;
 use log::Level::Info;
 use log::*;
@@ -13,7 +17,9 @@ use std::env::set_current_dir;
 use vite_actix::proxy_vite_options::ProxyViteOptions;
 use vite_actix::start_vite_server;
 
+mod arguments;
 mod auth;
+mod configuration;
 mod helpers;
 pub mod io;
 
@@ -23,8 +29,9 @@ pub async fn run() -> Result<()> {
         .format_timestamp(None)
         .init();
 
-    info!("Starting server...");
+    let args = FilerArguments::parse();
 
+    info!("Starting server...");
     if DEBUG {
         ProxyViteOptions::new().log_level(Info).build()?;
         std::thread::spawn(|| {
@@ -41,28 +48,40 @@ pub async fn run() -> Result<()> {
                 }
             }
         });
-        set_current_dir("target/wwwroot")?;
+        set_current_dir("target/dev-env")?;
     }
+
+    Configuration::set_path("app-config.json")?;
+    Configuration::load()?;
+    let config = Configuration::get();
+
+    let port = if args.port.is_some() {
+        args.port.unwrap()
+    } else {
+        config.port
+    };
 
     auth_db::initialize().await?;
 
     // Start file indexing and watcher in a separate task to avoid blocking server startup
-    if !IndexerData::does_table_exist().await? {
-        indexer_db::initialize().await?;
-        tokio::spawn(async {
-            if let Err(e) = indexer_data::index_all_files().await {
-                error!("Error starting file indexer: {}", e);
-            }
-        });
-    }
+    if !args.disable_indexing && config.indexing_enabled {
+        if !IndexerData::does_table_exist().await? {
+            indexer_db::initialize().await?;
+            tokio::spawn(async {
+                if let Err(e) = indexer_data::index_all_files().await {
+                    error!("Error starting file indexer: {}", e);
+                }
+            });
+        }
 
-    if !DEBUG {
-        tokio::spawn(async {
-            // Start file watcher
-            if let Err(e) = indexer_data::start_file_watcher().await {
-                error!("Error starting file watcher: {}", e);
-            }
-        });
+        if !args.disable_filewatchers && config.file_watcher_enabled {
+            tokio::spawn(async {
+                // Start file watcher
+                if let Err(e) = indexer_data::start_file_watcher().await {
+                    error!("Error starting file watcher: {}", e);
+                }
+            });
+        }
     }
     let server = HttpServer::new(move || {
         App::new()
@@ -91,13 +110,13 @@ pub async fn run() -> Result<()> {
             .configure_frontend_routes()
     })
     .workers(4)
-    .bind(format!("0.0.0.0:{port}", port = PORT))?
+    .bind(format!("0.0.0.0:{}", port))?
     .run();
 
     info!(
         "Starting {} server at http://127.0.0.1:{}...",
         if DEBUG { "development" } else { "production" },
-        PORT
+        port
     );
 
     let stop_result = server.await;
