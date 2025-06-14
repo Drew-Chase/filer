a622b515111a2cd48ad1a152a3a98605f679f3e4
Drew Chase <dcmanproductions@gmail.com>
2025-06-14 14:06:05 -0400

    Integrate `SetupProgressStepper` and `activeView` rendering into `SetupPage` with animations and layout improvements.

--- src/assets/pages/SetupPage.tsx
+++ src/assets/pages/SetupPage.tsx
@@ -1,8 +1,26 @@
+import SetupProgressStepper from "../components/setup/SetupProgressStepper.tsx";
+import {cn} from "@heroui/react";
+import {AnimatePresence, domAnimation, LazyMotion} from "framer-motion";
+import {useSetup} from "../providers/SetupProvider.tsx";
+
 export default function SetupPage()
 {
+    const {activeView} = useSetup();
     return (
-        <>
-        </>
+        <div className={"w-[90vw] h-[100dvh] gap-4 m-auto flex items-center"}>
+            <div className={
+                cn(
+                    "h-[80%] w-full flex-row flex gap-4"
+                    // "bg-white/5 rounded-xl shadow-xl p-6 border border-white/20"
+                )}>
+                <SetupProgressStepper/>
+                <LazyMotion features={domAnimation}>
+                    <AnimatePresence mode={"wait"}>
+                        {activeView}
+                    </AnimatePresence>
+                </LazyMotion>
+            </div>
+        </div>
     );
 }
 
